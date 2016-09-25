10c37eef356755f23a26d3c0b1da617010f8729b
Abhinav Garg <abhi.garg@okstate.edu>
2016-09-25 16:53:04 -0500

    Update randomSampling.R

--- randomSampling.R
+++ randomSampling.R
@@ -1,8 +1,10 @@
+# Author: Abhinav Garg
 randomSampling <- function(d, sampleSize)
 {
   #Setting seed value.
   set.seed(1234)
   #### Determine the indices each subset will have
   indices.samp = sort(sample(seq_len(nrow(d)), sampleSize))
+  ## Return the sample to calling object.
   d[indices.samp,]
 }
